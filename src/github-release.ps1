d292d6da3e86c576226e2eb843eb46b8dc850dba
Aliaksandr Tsoi <aliaksandr_tsoi@epam.com>
2014-10-29 17:43:07 +0300

    Updated test script file

--- src/github-release.ps1
+++ src/github-release.ps1
@@ -1,4 +1,4 @@
-gci "build_vcs_number"
-gci "BuildVcsNumber"
-gci "build_version"
-gci "BuildVersion"
+gci env:"build_vcs_number"
+gci env:"BuildVcsNumber"
+gci env:"build_version"
+gci env:"BuildVersion"
