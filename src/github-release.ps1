a7108f778dd0696061b5b4525db99bfb2520cf3c
Aliaksandr Tsoi <aliaksandr_tsoi@epam.com>
2014-10-30 19:10:54 +0300

    #1

--- src/github-release.ps1
+++ src/github-release.ps1
@@ -9,10 +9,9 @@ $json = @{ tag_name = $name; target_commitish = $hash; name = $name; body = ("Au
 $authorization = @{ Authorization = ("Basic {0}" -f [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $username, $password)))) }
 $response = Invoke-RestMethod "https://api.github.com/repos/altso/sandbox/releases" -Method Post -Headers $authorization -ContentType "application/json" -Body $json
 
-gci -Recurse
-
 Foreach ($artifact in $artifacts)
 {
+    $artifact
     $file = New-Object System.IO.FileInfo $artifact
     $uploadUrl = $response.upload_url -replace "\{\?name\}", ("?name={0}" -f $file.Name)
     Write-Host ("Uploading {0} to {1}..." -f $file.FullName, $uploadUrl)
